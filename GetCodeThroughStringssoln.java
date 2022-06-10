041d3c6ce19ab87a910411c87bf974500eb0be9e
Dhanush kumar k <93399149+Dhanushkumar24@users.noreply.github.com>
2022-06-10 20:04:10 +0530

    Create GetCodeThroughStringssoln.java

---
+++ GetCodeThroughStringssoln.java
@@ -0,0 +1,22 @@
+String words[]=input1.split(" ");
+        int length=words.length;
+		int result=0,sum=0;
+		for(int i=0;i<length;i++){
+			String text=words[i];
+			int len=text.length();
+			sum+=len;
+		}
+		while(sum>9){
+			int temp=sum%10;;
+            sum/=10;
+			result+=temp;
+		}
+		result+=sum;
+		sum=0;
+		while(result>9){
+			int temp=result%10;;
+            result/=10;
+			sum+=temp;
+		}
+		sum+=result;
+        return sum;

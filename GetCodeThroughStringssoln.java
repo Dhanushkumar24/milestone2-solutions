55207e88286527faab231c1e49768cd42dcb7d0c
Dhanush kumar k <93399149+Dhanushkumar24@users.noreply.github.com>
2022-06-10 20:21:38 +0530

    Update GetCodeThroughStringssoln.java

--- GetCodeThroughStringssoln.java
+++ GetCodeThroughStringssoln.java
@@ -1,6 +1,6 @@
-String words[]=input1.split(" ");
-        int length=words.length;
-		int result=0,sum=0;
+             String words[]=input1.split(" ");
+             int length=words.length;
+             int result=0,sum=0;
 		for(int i=0;i<length;i++){
 			String text=words[i];
 			int len=text.length();
@@ -8,15 +8,15 @@ String words[]=input1.split(" ");
 		}
 		while(sum>9){
 			int temp=sum%10;;
-            sum/=10;
+                        sum/=10;
 			result+=temp;
 		}
 		result+=sum;
 		sum=0;
 		while(result>9){
 			int temp=result%10;;
-            result/=10;
+                         result/=10;
 			sum+=temp;
 		}
 		sum+=result;
-        return sum;
+                return sum;

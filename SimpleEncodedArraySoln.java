f080866015ebcb64ed6cbd363a500b069484636d
Dhanush kumar k <93399149+Dhanushkumar24@users.noreply.github.com>
2022-06-11 12:48:46 +0530

    Create SimpleEncodedArraySoln.java

---
+++ SimpleEncodedArraySoln.java
@@ -0,0 +1,9 @@
+
+for(int i=input2-2;i>=0;i--){
+          input1[i]=input1[i]-input1[i+1];
+		}
+		int arr[]=input1; int sum=0;
+		for(int i=0;i<input2;i++){
+           sum+=input1[i];
+		}
+		return new Result(arr[0],sum);

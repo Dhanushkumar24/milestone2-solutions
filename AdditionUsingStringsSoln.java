12766946f647d71c67b4136e871fadc58b4013b7
Dhanush kumar k <93399149+Dhanushkumar24@users.noreply.github.com>
2022-06-27 19:44:04 +0530

    Update and rename AdditionUsingStrings.java to AdditionUsingStringsSoln.java

--- AdditionUsingStringsSoln.java
+++ AdditionUsingStringsSoln.java
@@ -1,6 +1,6 @@
    import java.math.*; // add at import section 
     
     BigDecimal x = new BigDecimal(input1);
-		BigDecimal y = new BigDecimal(input2);
+    BigDecimal y = new BigDecimal(input2);
 		
-		return String.valueOf(x.add(y));
+    return String.valueOf(x.add(y));
